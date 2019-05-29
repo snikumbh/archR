af92504128fa85eb0cdd45af6a00f267ffcb025a
snikumbh <snikumbh@github.com>
2019-05-29 13:49:50 +0100

    cleaned comments

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,15 @@
 
-sklearn <- NULL
-
 .onLoad <- function(libname, pkgname){
-  # use superassignment to update global reference to scipy
-  # sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
-  # reticulate::source_python('inst/python/perform_nmf.py')
-  # reticulate::source_python(system.file("extdata", "example_data.fa", package = "archR", mustWork = TRUE))
+
   reticulate::source_python(system.file("python/perform_nmf.py",
-                                        package="archR",
-                                        mustWork=TRUE))
+                                        package = "archR",
+                                        mustWork = TRUE))
   # CMD check NOTE avoidance
   # See: https://stackoverflow.com/a/12429344
   # Example: https://github.com/HughParsonage/grattan/blob/master/R/zzz.R
-    if(getRversion() >= "2.15.1"){
+  if (getRversion() >= "2.15.1") {
         utils::globalVariables(
-          c("Nucleotides", "X", "cvfolds", "perform_nmf_func", "positions", "q2_vals", "rel_var", "value"))
-    }
+          c("Nucleotides", "X", "cvfolds", "perform_nmf_func", "positions",
+            "q2_vals", "rel_var", "value"))
+  }
 }
