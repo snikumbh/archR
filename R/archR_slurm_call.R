818c8bc3894d225d3df44749c7f48fe440259b07
snikumbh <sarvesh.nikumbh@gmail.com>
2020-08-13 14:46:12 +0100

    adapted get_job_status from rslurm to access job dir at custom location [skip travis]

--- R/archR_slurm_call.R
+++ R/archR_slurm_call.R
@@ -170,3 +170,52 @@ archR_slurm_call <- function(f, params, jobname = NA, global_objects = NULL,
     archR::slurm_job(jobname, jobid, 1)
 }
 
+#' Date: 2020-06-27
+#' Author: Sarvesh Nikumbh
+#'
+#' This is an adapted version of the slurm_call function from rslurm package.
+#' Changes implemented:
+#' 1. Access custom directory created at custom location specified by the user
+#'
+#'
+#' Get the status of a Slurm job
+#'
+#' This function returns the completion status of a Slurm job, its queue status
+#' if any and log outputs.
+#'
+#' The \code{queue} element of the output is a data frame matching the output
+#' of the Slurm \code{squeue} command for that job; it will only indicate portions
+#' of job that are running or in queue. The \code{log} element is a
+#' vector of the contents of console/error output files for each node where the
+#' job is running.
+#'
+#' @importFrom utils read.table
+#' @param slr_job A \code{slurm_job} object.
+#' @return A list with three elements: \code{completed} is a logical value
+#'   indicating if all portions of the job have completed or stopped, \code{queue}
+#'   contains the information on job elements still in queue, and
+#'   \code{log} contains the console/error logs.
+#' @export
+archR_get_job_status <- function(slr_job, dirName) {
+
+    if (!(class(slr_job) == "slurm_job")) stop("input must be a slurm_job")
+
+    # Get queue info
+    squeue_out <- suppressWarnings(
+        system(paste("squeue -n", slr_job$jobname), intern = TRUE)
+    )
+    queue <- read.table(text = squeue_out, header = TRUE)
+    completed <- nrow(queue) == 0
+
+    # Get output logs
+    tmpdir <- dirName #paste0("_rslurm_", slr_job$jobname)
+    out_files <- file.path(tmpdir, paste0("slurm_", 0:(slr_job$nodes - 1), ".out"))
+    logs <- vapply(out_files,
+                   function(outf) paste(readLines(outf), collapse = "\n"),
+                   "")
+
+    job_status <- list(completed = completed, queue = queue, log = logs)
+    class(job_status) <- "slurm_job_status"
+    job_status
+}
+
