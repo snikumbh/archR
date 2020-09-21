8d9e2fd77c43663938d94541226703b0a06f3114
snikumbh <sarvesh.nikumbh@gmail.com>
2020-09-21 02:45:18 +0100

    tests for archR_main, stability- and cv-based (this is skipped on travis)

--- tests/testthat/test_archR_main.R
+++ tests/testthat/test_archR_main.R
@@ -2,49 +2,93 @@
 
 context("archR main functionality")
 
-# library(archR)
-# reticulate::source_python(system.file("python/perform_nmf.py",
-#                                       package = "archR",
-#                                       mustWork = TRUE)
-# )
-# seed_val <- 11992288
-# set.seed(seed_val)
 ## Make toy objects and data
 inputFastaFilename <- system.file("extdata", "example_data.fa",
                                   package = "archR",
                                   mustWork = TRUE)
-tssSeqsOriginal <- archR::prepare_data_from_FASTA(inputFastaFilename)
+tssSeqs <- suppressMessages(archR::prepare_data_from_FASTA(inputFastaFilename))
+tssSeqsRaw <- suppressMessages(archR::prepare_data_from_FASTA(inputFastaFilename, rawSeq = TRUE))
 
-nSeqs <- ncol(tssSeqsOriginal)
-positions <- seq(1,200)
-sinuc <- c('A', 'C', 'G', 'T')
-changedOrder <- sample.int(nSeqs, nSeqs, replace = FALSE)
-tssSeqs <- tssSeqsOriginal[ , changedOrder]
-toyConfig <- archRSetConfig(innerChunkSize = 500,
-                            kMin = 2, kMax = 5, parallelize = FALSE,
-                            cvFolds = 3, nIterationsUse = 5,
+nSeqs <- ncol(tssSeqs)
+positions <- seq(1,100)
+# sinuc <- c('A', 'C', 'G', 'T')
+# changedOrder <- sample.int(nSeqs, nSeqs, replace = FALSE)
+# tssSeqs <- tssSeqsOriginal[ , changedOrder]
+toyConfig <- archR::archRSetConfig(innerChunkSize = 100,
+                            kMin = 2, kMax = 20, parallelize = FALSE,
+                            modSelType = "stability", 
+                            nIterationsUse = 50,
                             nCoresUse = NA)
 
 
-# test_that("archR works", {
-#     toyResult <- archR::archR(toyConfig, seqsMat = tssSeqs, thresholdItr = 2)
-#     expect_identical(toyResult, archRresult)
-# })
+test_that("archR works", {
+    set.seed(1234)
+    archRresult <- suppressMessages(archR::archR(toyConfig, seqsRaw = tssSeqsRaw, 
+                                seqsMat = tssSeqs, thresholdItr = 1))
+    ##
+    ## We just the seqsClustLabels and a block of the basis vectors matrix
+    # toyResult_seqsClustLabels <- 
+    #     c("1", "1", "2", "3", "2", "1", "2", "2", "3", "2", "1", "2", "2", "3", 
+    #       "3", "2", "2", "2", "2", "1", "3", "2", "1", "3", "2", "3", "3", "3", 
+    #       "3", "2", "2", "1", "3", "3", "3", "2", "2", "2", "2", "3", "2", "3", 
+    #       "2", "3", "2", "1", "2", "3", "2", "1", "2", "1", "2", "2", "2", "2", 
+    #       "3", "2", "2", "2", "2", "3", "1", "2", "3", "3", "2", "1", "2", "2", 
+    #       "1", "1", "2", "3", "1", "3", "2", "3", "3", "1", "3", "1", "1", "2", 
+    #       "1", "1", "1", "1", "2", "3", "2", "1", "3", "2", "2", "3", "3", "2", 
+    #       "1", "3", "3", "1", "2", "1", "1", "2", "2", "3", "2", "3", "3", "1", 
+    #       "3", "1", "2", "1", "2", "2", "2", "2", "2", "2", "1", "3", "2", "2", 
+    #       "3", "2", "3", "2", "1", "2", "2", "2", "2", "2", "1", "1", "3", "2", 
+    #       "2", "2", "1", "2", "1", "1", "1", "1", "3", "2", "1", "2", "2", "2", 
+    #       "3", "2", "3", "1", "2", "2", "2", "2", "3", "2", "1", "2", "1", "1", 
+    #       "3", "3", "1", "3", "2", "3", "2", "1", "2", "3", "1", "2", "3", "1", 
+    #       "2", "2", "2", "2", "3", "2", "2", "1", "2", "3", "2", "2", "2", "2", 
+    #       "1", "2", "2", "2")
+    # toyResult_nBasisVectors <- 3
+    # toyResult_factors_10_3 <- matrix(c(0.141375993, 0.23189513, 0.31949721,
+    #                             0.129934837, 0.26171279, 0.23964413,
+    #                             0.162060017, 0.23019522, 0.30487274,
+    #                             0.150710132, 0.18659361, 0.25073772,
+    #                             0.187403520, 0.31464845, 0.26935487), 
+    #                             nrow = 10, ncol = 3, byrow = TRUE)
+    ##
+    # expect_identical(toyResult, archRresult)
+    # expect_equal(archRresult$seqsClustLabels[[1]], toyResult_seqsClustLabels)
+    # expect_equal(archRresult$clustBasisVectors[[1]]$nBasisVectors, toyResult_nBasisVectors)
+    # expect_equal(archRresult$clustBasisVectors[[1]]$basisVectors[1:10,], 
+    #              toyResult_factors_10_3)
+    expect_equal_to_reference(archRresult, "archRresult_stability_check.rds")
+    ##
+    ## Test cross-validation-based model selection. This needs to parallel as TRUE.
+    toyConfig <- archR::archRSetConfig(innerChunkSize = 100,
+                            kMin = 2, kMax = 20, parallelize = TRUE,
+                            modSelType = "cv",
+                            nIterationsUse = 10,
+                            nCoresUse = 2,
+                            flags = list(debugFlag = FALSE,
+                                         verboseFlag = TRUE,
+                                         plotVerboseFlag = FALSE,
+                                         timeFlag = TRUE))
+    set.seed(1234)
+    archRresult <- archR::archR(toyConfig, seqsRaw = tssSeqsRaw,
+                                seqsMat = tssSeqs, thresholdItr = 1)
+    skip_on_travis()
+    expect_equal_to_reference(archRresult, "archRresult_cv_check.rds")
+})
 
 
 test_that("Handles negative threshold iteration", {
     # toyResult <- archR(toyConfig, seqsMat = tssSeqs, thresholdItr = -1)
     expect_error(.assert_archR_thresholdIteration(-1),
                  "Expecting threshold iteration to be numeric and > 0")
-    expect_error(archR(toyConfig, seqsMat = tssSeqs, thresholdItr = -1),
+    expect_error(archR(toyConfig, seqsRaw = tssSeqsRaw, seqsMat = tssSeqs, thresholdItr = -1),
                  "Expecting threshold iteration to be numeric and > 0")
 })
 
-# test_that("Handles negative threshold iteration from archR main", {
-#     expect_error(archR(toyConfig, seqsMat = tssSeqs, thresholdItr = -1))
-#     # expect_error(archR(toyConfig, seqsMat = tssSeqs, thresholdItr = -1),
-#     #              "Expecting threshold iteration to be numeric and > 0")
-# })
+test_that("Handles negative threshold iteration from archR main", {
+    expect_error(archR(toyConfig, seqsRaw = tssSeqsRaw, seqsMat = tssSeqs, thresholdItr = -1))
+    expect_error(archR(toyConfig, seqsRaw = tssSeqsRaw, seqsMat = tssSeqs, thresholdItr = -1),
+                 "Expecting threshold iteration to be numeric and > 0")
+})
 
 
 test_that("Config handles: negative innerChunkSize", {
