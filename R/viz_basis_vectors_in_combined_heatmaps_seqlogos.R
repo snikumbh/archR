da1c99c789610f4ad47462b784261784947f9095
snikumbh <sarvesh.nikumbh@gmail.com>
2021-01-02 19:08:08 +0000

    moved ggpubr to suggests/func checks requireNamespace

--- R/viz_basis_vectors_in_combined_heatmaps_seqlogos.R
+++ R/viz_basis_vectors_in_combined_heatmaps_seqlogos.R
@@ -35,54 +35,58 @@ viz_basis_vectors_in_combined_heatmaps_seqlogos <-
     function(featuresMatrix, plotMethod = "custom", position_labels = NA,
                 add_pseudo_counts = FALSE, savePDFfilename = NULL,
                 sinuc_or_dinuc = "sinuc") {
-        ##
-    if (!is.matrix(featuresMatrix)) {
-        stop("featuresMatrix not of type matrix")
-    }
-    if (sum(dim(featuresMatrix)) == 2 && is.na(featuresMatrix)) {
-        stop("Empty featuresMatrix")
-    }
-    invisible(apply(featuresMatrix, MARGIN = 2, function(x) {
-        if (sinuc_or_dinuc == "dinuc") {
-            dna_alphabet <- c("A", "C", "G", "T")
-            dna_alphabet_dinuc <- do.call(paste0, expand.grid(dna_alphabet,
-                                                                dna_alphabet))
-            pwm <- make_dinuc_PWMs(as.matrix(x), add_pseudo_counts = FALSE,
-                                scale = FALSE)
-        } else if (sinuc_or_dinuc == "sinuc") {
-            pwm <- make_sinuc_PWMs(x, add_pseudo_counts = FALSE, scale = FALSE)
+    ##
+    if(!requireNamespace("ggpubr", quietly = TRUE)){
+        stop("Please install the R package 'ggpubr' to use this function")
+    }else{
+        if (!is.matrix(featuresMatrix)) {
+            stop("featuresMatrix not of type matrix")
         }
-        ## Heatmap on top
-        p1 <- plot_ggheatmap(
-            pwmMat = pwm,
-            position_labels = position_labels,
-            savePDFfilename = savePDFfilename
-        )
-        p1 + theme(
-            plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
-        )
-        ## Seqlogo below
-        p2 <- plot_ggseqlogo(
-            pwmMat = pwm,
-            plotMethod = plotMethod,
-            position_labels = position_labels,
-            savePDFfilename = savePDFfilename
-        )
-        ## Make adjustments for alignment
-        p2 + theme(
-            plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
-        )
-        final_p <- ggpubr::ggarrange(p1, p2, ncol = 1, heights = c(1,1),
-                            widths = c(1,1), align = 'v')
-        ##
-        if (!is.null(savePDFfilename)) {
-            if (file.exists(savePDFfilename)) {
-                warning("File exists, will overwrite", immediate. = TRUE)
+        if (sum(dim(featuresMatrix)) == 2 && is.na(featuresMatrix)) {
+            stop("Empty featuresMatrix")
+        }
+        invisible(apply(featuresMatrix, MARGIN = 2, function(x) {
+            if (sinuc_or_dinuc == "dinuc") {
+                dna_alphabet <- c("A", "C", "G", "T")
+                dna_alphabet_dinuc <- do.call(paste0, expand.grid(dna_alphabet,
+                                                                    dna_alphabet))
+                pwm <- make_dinuc_PWMs(as.matrix(x), add_pseudo_counts = FALSE,
+                                    scale = FALSE)
+            } else if (sinuc_or_dinuc == "sinuc") {
+                pwm <- make_sinuc_PWMs(x, add_pseudo_counts = FALSE, scale = FALSE)
             }
-            ggplot2::ggsave(final_p, device = "pdf", width = 20, height = 2.5)
-        } else {
-            base::suppressMessages(print(final_p))
-        }
-    }))
+            ## Heatmap on top
+            p1 <- plot_ggheatmap(
+                pwmMat = pwm,
+                position_labels = position_labels,
+                savePDFfilename = savePDFfilename
+            )
+            p1 + theme(
+                plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
+            )
+            ## Seqlogo below
+            p2 <- plot_ggseqlogo(
+                pwmMat = pwm,
+                plotMethod = plotMethod,
+                position_labels = position_labels,
+                savePDFfilename = savePDFfilename
+            )
+            ## Make adjustments for alignment
+            p2 + theme(
+                plot.margin = grid::unit(c(0, 0, 0, 0), "mm")
+            )
+            final_p <- ggpubr::ggarrange(p1, p2, ncol = 1, heights = c(1,1),
+                                widths = c(1,1), align = 'v')
+            ##
+            if (!is.null(savePDFfilename)) {
+                if (file.exists(savePDFfilename)) {
+                    warning("File exists, will overwrite", immediate. = TRUE)
+                }
+                ggplot2::ggsave(final_p, device = "pdf", width = 20, height = 2.5)
+            } else {
+                base::suppressMessages(print(final_p))
+            }
+        }))
+    }
 }
 ## =============================================================================
