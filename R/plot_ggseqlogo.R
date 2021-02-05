2e59e998ba05e879a09ef5cebf619a0ff783a597
snikumbh <sarvesh.nikumbh@gmail.com>
2021-02-05 17:45:12 +0000

    changed to underscore based arg naming for consistency + minor adjustments

--- R/plot_ggseqlogo.R
+++ R/plot_ggseqlogo.R
@@ -1,24 +1,31 @@
-
-
-#' @title Visualize a given (PWM) matrix as a sequence logo
+#' @title Visualize a given (PWM) matrix as a sequence logo.
 #'
-#' @param pwmMat Matrix (usually a PWM, but can be any non-normalized matrix) to
-#'  be represented as a sequence logo.
-#' @param plotMethod For \code{ggseqlogo}; either of 'custom', 'bits', or
+#' @param pwm_mat Matrix (usually a PWM, but can be any non-normalized matrix) 
+#' to be represented as a sequence logo.
+#' 
+#' @param plot_method For \code{ggseqlogo}; either of 'custom', 'bits', or
 #' 'probability'. Default is 'custom'.
-#' @param position_labels Labels of the positions in the sequences.
-#' @param savePDFfilename Name of the file which will be saved as PDF.
+#' 
+#' @param pos_lab Labels of the positions in the sequences.
+#' 
+#' @param pdf_fname Name of the file which will be saved as PDF.
 #'
-#' @return A ggplot object so you can simply call \code{print} or \code{save}
-#' on it later. If \code{savePDFfilename} is given, it is also saved and the
-#' \code{ggplot} object returned.
+#' @return A ggplot2 object so you can simply call \code{print} or \code{save}
+#' on it later. If \code{pdf_fname} is given, it is also saved in addition to 
+#' returning the ggplot object.
+#' 
 #' @export
+#' 
 #' @family visualization functions
-#' @seealso \code{\link{plot_ggheatmap}} for plotting PWMs as heatmaps
+#' 
+#' @seealso \code{\link{plot_ggheatmap}} for plotting PWMs as heatmaps,
+#' \code{\link{plot_ggseqlogo_of_seqs}} for visualizing a collection of 
+#' sequences by their sequence logo.
+#' 
 #' @import ggplot2
 #' @import ggseqlogo
-plot_ggseqlogo <- function(pwmMat, plotMethod = "custom",
-                            position_labels = NULL, savePDFfilename = NULL) {
+plot_ggseqlogo <- function(pwm_mat, plot_method = "custom",
+                            pos_lab = NULL, pdf_fname = NULL) {
     ##
     if (!is.matrix(pwmMat)) {
         stop("Expecting a matrix with 4 rows")
@@ -45,24 +52,18 @@ plot_ggseqlogo <- function(pwmMat, plotMethod = "custom",
     }
     ##
     p1 <- ggplot() +
-        geom_logo(pwmMat, method = plotMethod, seq_type = "dna") +
-        theme_logo() +
-        # theme_update(plot.margin=grid::unit(c(0,0,0,0), 'mm'),
-        # axis.ticks.length =
-        # unit(0, 'lines'), axis.line = element_blank(),
-        # axis.title.y = element_blank(),
-        # axis.text.y = element_blank() ) + theme_bw() +
-        ggplot2::scale_x_continuous(breaks = seq_len(ncol(pwmMat)),
-                                    labels = position_labels,
-                                    expand = expansion(mult = c(0, 0))) +
-        # ggplot2::coord_fixed(ratio = 8, clip = 'on') +
-        # ggplot2::labs(title = plot.title)
-        # +
-        ggplot2::theme(axis.text.x = element_text(size = rel(0.5),
-                                                    angle = 90,
-                                                    hjust = 1),
-                        axis.text.y = element_text(size = rel(0.5))) +
-        ggplot2::ylim(0.0, 2.0)
+            geom_logo(pwmMat, method = plotMethod, seq_type = "dna") +
+            theme_logo() +
+            ##
+            ggplot2::scale_x_continuous(breaks = seq_len(ncol(pwmMat)),
+                                        labels = position_labels,
+                                        expand = expansion(mult = c(0, 0))) +
+            ##
+            ggplot2::theme(axis.text.x = element_text(size = rel(0.5),
+                                                angle = 90, hjust = 1),
+                            axis.text.y = element_text(size = rel(0.5))) +
+            ggplot2::ylim(0.0, 2.0)
+    ##
     if (!is.null(savePDFfilename)) {
         if (file.exists(savePDFfilename)) {
             warning("File exists, will overwrite", immediate. = TRUE)
