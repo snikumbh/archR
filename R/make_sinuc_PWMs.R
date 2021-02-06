b73ac0e461df85765035309fcc4ff33d87f15892
snikumbh <sarvesh.nikumbh@gmail.com>
2021-02-06 02:32:53 +0000

    updated funcs for standardization

--- R/make_sinuc_PWMs.R
+++ R/make_sinuc_PWMs.R
@@ -3,7 +3,7 @@
 #' @description The given matrix (or simply a vector) is reshaped to have four
 #' rows for four nucleotides and a relevant number of columns.
 #'
-#' @param givenMatrix Actually a vector that will be reshaped into a (PWM)
+#' @param mat Actually a vector that will be reshaped into a (PWM)
 #' matrix of DNA sequences.
 #' @param add_pseudo_counts Boolean, taking values TRUE/T or FALSE/F, specifying
 #' whether or not pseudocounts are added to the matrix.
@@ -15,29 +15,29 @@
 #' vector/4)
 #'
 #' @export
-make_sinuc_PWMs <- function(givenMatrix, add_pseudo_counts = TRUE,
+make_sinuc_PWMs <- function(mat, add_pseudo_counts = TRUE,
                             scale = TRUE) {
     ## return PWM matrix TO-DO: Make more clear whether a matrix or a column
     ## vector is expected
-    ## if(!is.matrix(givenMatrix)){
-    ## stop('givenMatrix not of type matrix') }
-    ## if(sum(dim(givenMatrix)) == 2 && is.na(givenMatrix)){
-    ## stop('Empty givenMatrix')
+    ## if(!is.matrix(mat)){
+    ## stop('mat not of type matrix') }
+    ## if(sum(dim(mat)) == 2 && is.na(mat)){
+    ## stop('Empty mat')
     ## }
     sinuc <- c("A", "C", "G", "T")
     if (add_pseudo_counts) {
-        givenMatrix <- givenMatrix + 10^-5
+        mat <- mat + 10^-5
     }
-    this_givenMatrix <- t(matrix(givenMatrix, ncol = length(sinuc),
+    this_mat <- t(matrix(mat, ncol = length(sinuc),
                                     byrow = FALSE))
-    rownames(this_givenMatrix) <- sinuc
+    rownames(this_mat) <- sinuc
     if (scale) {
-        scaled <- this_givenMatrix
-        scaled <- base::sweep(this_givenMatrix, 2, colSums(this_givenMatrix),
+        scaled <- this_mat
+        scaled <- base::sweep(this_mat, 2, colSums(this_mat),
                                 "/")
         return(scaled)
     } else {
-        return(this_givenMatrix)
+        return(this_mat)
     }
 }
 
@@ -49,37 +49,41 @@ make_sinuc_PWMs <- function(givenMatrix, add_pseudo_counts = TRUE,
 #' of dinucleotide information into matrix of dimension 
 #' 16 x (sequence_length) for visualization.
 #' 
-#' @param givenMatrix Input basis matrix
+#' @param mat Input basis matrix
 #' @param add_pseudo_counts Whether pesudocounts are to be added. TRUE or FALSE.
 #' @param scale Whether to perform per position scaling of the matrix. TRUE or 
 #' FALSE
 #' 
+#' @return A (PWM) matrix with 4 rows corresponding to the 4 nucleotides (A, C,
+#' G, T) and the relevant number of columns (i.e., number of elements in given
+#' vector/4)
+#' 
 #' @export
-make_dinuc_PWMs <- function(givenMatrix, add_pseudo_counts = TRUE,
+make_dinuc_PWMs <- function(mat, add_pseudo_counts = TRUE,
                             scale = TRUE) {
     # return PWM matrix TO-DO: Make more clear whether a matrix or a colum
     # vector is expected
-    # if(!is.matrix(givenMatrix)){
-    # stop('givenMatrix not of type matrix')
+    # if(!is.matrix(mat)){
+    # stop('mat not of type matrix')
     # }
-    # if(sum(dim(givenMatrix)) == 2 && is.na(givenMatrix)){
-    # stop('Empty givenMatrix')
+    # if(sum(dim(mat)) == 2 && is.na(mat)){
+    # stop('Empty mat')
     # }
     dna_alphabet <- c("A", "C", "G", "T")
     dinuc <- do.call(paste0, expand.grid(dna_alphabet, dna_alphabet))
     if (add_pseudo_counts) {
-        givenMatrix <- givenMatrix + 10^-5
+        mat <- mat + 10^-5
     }
-    this_givenMatrix <- t(matrix(givenMatrix, ncol = length(dinuc),
+    this_mat <- t(matrix(mat, ncol = length(dinuc),
                                     byrow = FALSE))
-    rownames(this_givenMatrix) <- dinuc
+    rownames(this_mat) <- dinuc
     if (scale) {
-        scaled <- this_givenMatrix
-        scaled <- base::sweep(this_givenMatrix, 2, colSums(this_givenMatrix),
+        scaled <- this_mat
+        scaled <- base::sweep(this_mat, 2, colSums(this_mat),
                                 "/")
         return(scaled)
     } else {
-        return(this_givenMatrix)
+        return(this_mat)
     }
 }
 
