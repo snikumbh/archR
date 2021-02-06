0c32507ac93c8d3fb3e693d51fff7393d4539aa7
snikumbh <sarvesh.nikumbh@gmail.com>
2021-02-06 03:15:04 +0000

    data files now unused -- removed

--- R/data.R
+++
@@ -1,8 +0,0 @@
-#' Example data matrix
-#'
-#' An example data matrix generated from example_data.fa FASTA file. This is a
-#' collection of 1000 sequences. There are four clusters present in them. The
-#' vignette explains this further.
-#'
-#' @usage data(example_data)
-"example_data"
--- data-raw/example_data.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `example_data` dataset goes here
-
-require(seqinr)
-inputFastaFilename <- file.path("data-raw/samarth-dataset-2-4clusters-ZeroMutation.fa")
-example_data <- archR::prepare_data_from_FASTA(inputFastaFilename)
-usethis::use_data(example_data)
